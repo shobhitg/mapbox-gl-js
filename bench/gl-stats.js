2cb342c10737369fa9f79920eacd2f51b9000472
Vladimir Agafonkin <agafonkin@gmail.com>
2019-06-27 12:13:41 +0300

    run puppeteer without sandboxing

--- bench/gl-stats.js
+++ bench/gl-stats.js
@@ -52,7 +52,9 @@ function waitForConsole(page) {
 }
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
 
     console.log('collecting stats...');
